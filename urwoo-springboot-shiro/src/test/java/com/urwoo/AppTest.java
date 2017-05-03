32e00fb90744cda3df34493264e0771ab5e817b8
yujin <1131473289@qq.com>
2017-05-04 00:29:45 +0800

    add

--- urwoo-springboot-shiro/src/test/java/com/urwoo/AppTest.java
+++ urwoo-springboot-shiro/src/test/java/com/urwoo/AppTest.java
@@ -1,38 +1,5 @@
 package com.urwoo;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
-
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+public class AppTest {
+    //https://github.com/511098425/springbootShiroRedis/blob/master/springShiro/src/main/java/com/boot/config/shiro/ShiroCasConfiguration.java
 }
